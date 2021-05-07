e6a148b31fe7e0543a4ecb04a303bdb86633c272
Shysonata <michael.cowled@gmail.com>
2021-05-07 14:52:27 +0800

    following computation of all heatmaps

--- heatmaps.R
+++ heatmaps.R
@@ -1,24 +1,31 @@
-filenames <- list.files(path = "Testing Broad-Scale Interactions/OutputFiles/Tal_FvF", pattern = "F", full.names = TRUE)
+filenames <- list.files(path = "Testing Broad-Scale Interactions/OutputFiles/Tal_FvA", pattern = "F", full.names = TRUE)
 my.data <- lapply(filenames, read.csv)
 full.list <- rbindlist(my.data, use.names=TRUE, fill=FALSE)
-full.list <- transform(full.list, Metabolite_Effect = factor(Metabolite_Effect))
-ALEI <- filter(full.list, as.numeric(Metabolite_Effect) < 5 & as.numeric(Metabolite_Effect) > 1)
-Cocultures <- c(unique(ALEI$Sample_Ref))
 
-ratio.df <- data.frame(matrix(ncol = 4, nrow = 0))
+#For changing NT to F in NT_FvA
+full.list$Sample_Ref <- gsub("NT", "F", full.list$Sample_Ref, perl = TRUE)
+full.list$Sample_Ref <- gsub("F17", "B1", full.list$Sample_Ref, perl = TRUE)
+full.list$Sample_Ref <- gsub("F18", "B2", full.list$Sample_Ref, perl = TRUE)
+full.list$Sample_Ref <- gsub("F19", "B3", full.list$Sample_Ref, perl = TRUE)
+
+refined.list <- filter(full.list, Metabolite_Effect < 6 & Metabolite_Effect > 1)
+Cocultures <- c(unique(refined.list$Sample_Ref))
+
+ratio.df <- data.frame(matrix(ncol = 3, nrow = 0))
 for (i in Cocultures[1:length(Cocultures)]) {
     print(i)
-    temp <- filter(ALEI, Sample_Ref == i)
+    temp <- filter(refined.list, Sample_Ref == i)
     ratio.df <- rbind(ratio.df, 
                       c(Sample_Ref = i, Max_Ratio = max(temp$PeakRatio), 
-                        Min_Ratio = min(temp$PeakRatio), Avg_Ratio = mean(temp$PeakRatio)))
+                        Min_Ratio = min(temp$PeakRatio)))
 }
 
-names(ratio.df) <- c("Sample_Ref", "Max_Ratio", "Min_Ratio", "Avg_Ratio")
+names(ratio.df) <- c("Sample_Ref", "Max_Ratio", "Min_Ratio")
 ratio.df <- separate(ratio.df, Sample_Ref, into = c("Ref_Culture", "Int_Culture"), sep = "v")
 ratio.df$Max_Ratio <- as.numeric(ratio.df$Max_Ratio)
 ratio.df$Min_Ratio <- as.numeric(ratio.df$Min_Ratio)
-ratio.df$Avg_Ratio <- as.numeric(ratio.df$Avg_Ratio)
+
+ratio.df <- mutate(ratio.df, Log_Max_Ratio = log(Max_Ratio))
 
 #For NT fungi
 ratio.df$Ref_Culture <- gsub("(?<![0-9])([0-9])(?![0-9])", "0\\1", ratio.df$Ref_Culture, perl = TRUE)
@@ -34,6 +41,16 @@ heatmap.fvf.enhancements <- ggplot(data = ratio.df, aes(x=Int_Culture, y=Ref_Cul
 heatmap.fvf.enhancements
 ggsave(heatmap.fvf.enhancements,filename="heatmap.fvf.enhancements.png",height=2.24,width=3,units="in",dpi=200)
 
+heatmap.fvf.log.enhancements <- ggplot(data = ratio.df, aes(x=Int_Culture, y=Ref_Culture)) + 
+    geom_tile(aes(fill=Log_Max_Ratio)) +
+    scale_fill_gradient2(low = "white", high = "dark red", mid = "white", 
+                         midpoint = 0, space = "Lab", name="log Ratio", limits = c(0, 10)) +
+    labs(x="Interacting Culture", y="Reference Culture") +
+    theme_grey(base_size=8) +
+    theme(axis.text.x=element_text(angle=45, hjust = 1))
+heatmap.fvf.enhancements
+ggsave(heatmap.fvf.log.enhancements,filename="heatmap.fvf.log.enhancements.png",height=2.24,width=3,units="in",dpi=200)
+
 heatmap.fvf.suppressions <- ggplot(data = ratio.df, aes(x=Int_Culture, y=Ref_Culture)) + 
     geom_tile(aes(fill=Min_Ratio)) +
     scale_fill_gradient2(low = "dark blue", high = "dark red", mid = "white", 
@@ -43,6 +60,30 @@ heatmap.fvf.suppressions <- ggplot(data = ratio.df, aes(x=Int_Culture, y=Ref_Cul
     theme(axis.text.x=element_text(angle=45, hjust = 1))
 heatmap.fvf.suppressions
 ggsave(heatmap.fvf.suppressions,filename="heatmap.fvf.suppressions.png",height=2.24,width=3,units="in",dpi=200)
+
+#Avg Ratio Heatmap
+
+refined.list <- filter(full.list, Metabolite_Effect < 5 & Metabolite_Effect > 1)
+cond.5.list <- filter(full.list, Metabolite_Effect == 5)
+cond.5.list$PeakRatio <- 100
+refined.list <- rbind(refined.list, cond.5.list)
+Cocultures <- c(unique(refined.list$Sample_Ref))
+
+ratio.df <- data.frame(matrix(ncol = 2, nrow = 0))
+for (i in Cocultures[1:length(Cocultures)]) {
+    print(i)
+    temp <- filter(refined.list, Sample_Ref == i)
+    ratio.df <- rbind(ratio.df, 
+                      c(Sample_Ref = i, Avg_Ratio = mean(temp$PeakRatio)))
+}
+
+names(ratio.df) <- c("Sample_Ref", "Avg_Ratio")
+ratio.df <- separate(ratio.df, Sample_Ref, into = c("Ref_Culture", "Int_Culture"), sep = "v")
+ratio.df$Avg_Ratio <- as.numeric(ratio.df$Avg_Ratio)
+
+#For NT fungi
+ratio.df$Ref_Culture <- gsub("(?<![0-9])([0-9])(?![0-9])", "0\\1", ratio.df$Ref_Culture, perl = TRUE)
+ratio.df$Int_Culture <- gsub("(?<![0-9])([0-9])(?![0-9])", "0\\1", ratio.df$Int_Culture, perl = TRUE)
 
 heatmap.fvf.avg <- ggplot(data = ratio.df, aes(x=Int_Culture, y=Ref_Culture)) + 
     geom_tile(aes(fill=Avg_Ratio)) +
